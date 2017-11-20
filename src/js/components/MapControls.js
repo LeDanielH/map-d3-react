f57650c53d1fc0fdcdad3bafe58046e28628b75d
daniel.zeman <daniel.zeman@socialbakers.com>
2017-11-20 11:57:48 +0100

    core: keeping fn naming consistent

--- src/js/components/MapControls.js
+++ src/js/components/MapControls.js
@@ -8,7 +8,7 @@ class MapControls extends Component {
 	}
 
 	render() {
-		const {handleZoomIn, handleZoomOut, pan, handleReset} = this.props;
+		const {handleZoomIn, handleZoomOut, handlePan, handleReset} = this.props;
 		return (
 			<div className={"rsm-controls"}>
 				<div className={'rsm-controls__zoom'}>
@@ -16,10 +16,10 @@ class MapControls extends Component {
 					<button onClick={handleZoomOut}>zoom out</button>
 				</div>
 				<div className={'rsm-controls__pan'}>
-					<button onClick={() => pan('up')}>up</button>
-					<button onClick={() => pan('right')}>right</button>
-					<button onClick={() => pan('down')}>bottom</button>
-					<button onClick={() => pan('left')}>left</button>
+					<button onClick={() => handlePan('up')}>up</button>
+					<button onClick={() => handlePan('right')}>right</button>
+					<button onClick={() => handlePan('down')}>bottom</button>
+					<button onClick={() => handlePan('left')}>left</button>
 				</div>
 				<div className={"rsm-controls__reset"}>
 					<button onClick={handleReset}>reset</button>
