25cf674e7de8a147a402fe652b82726bc473a1c2
daniel.zeman <daniel.zeman@socialbakers.com>
2017-11-21 14:37:07 +0100

    core: added helper for converting long lat to dms

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -39,3 +39,21 @@ export function getClickHandler(onClick, onDoubleClick, delay) {
 		}
 	};
 }
+
+function toDegreesMinutesAndSeconds(coordinate) {
+	const absolute = Math.abs(coordinate);
+	const degrees = Math.floor(absolute);
+	const minutesNotTruncated = (absolute - degrees) * 60;
+	const minutes = Math.floor(minutesNotTruncated);
+	const seconds = Math.floor((minutesNotTruncated - minutes) * 60);
+	return `${degrees}°${minutes}'${seconds}"`;
+}
+
+export function convertDMS(lat, lng) {
+	const latitude = toDegreesMinutesAndSeconds(lat);
+	const latitudeCardinal = Math.sign(lat) >= 0 ? "N" : "S";
+	const longitude = toDegreesMinutesAndSeconds(lng);
+	const longitudeCardinal = Math.sign(lng) >= 0 ? "E" : "W";
+	const dms = `${latitude}${latitudeCardinal}+${longitude}${longitudeCardinal}`;
+	return dms;
+}
